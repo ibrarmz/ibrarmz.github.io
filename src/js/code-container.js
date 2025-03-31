666601b754c10607f0bcdf6eaad2aa9f23c6dd49
ibra <ibra@debian.ipv6.infinitum.net.mx>
2025-03-30 20:18:45 -0600

    First Commit

---
+++ src/js/code-container.js
@@ -0,0 +1,17 @@
+const codeBlock = document.getElementById("code-block");
+const lineNumbers = document.getElementById("line-numbers");
+
+// Actualizar los números de línea
+function updateLineNumbers() {
+    let lineNumberText = "";
+
+    // Crear los números de línea
+    for (let i = 1; i <= 18; i++) {
+        lineNumberText += `${i}\n`;
+    }
+
+    lineNumbers.innerText = lineNumberText;
+}
+
+// Llamar a la función al cargar la página
+updateLineNumbers();
